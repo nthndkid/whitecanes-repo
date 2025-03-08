94bb8b9d09b62f36bec91dffd57943e38160a794
nthndkid <nathanflrs813@gmail.com>
2025-03-08 19:26:27 +0800

    added alert

---
+++ script.js
@@ -0,0 +1,8 @@
+// const btn = document.querySelector('btn');
+// btn.addEventListener('click', () => {
+//     alert("Flores - Developer, Rosales - Designer, Macas - Database Administrator")
+// });
+
+document.getElementById('btn').addEventListener("click", function(){
+    alert("Flores - Developer, Rosales - Designer, Macas - Database Administrator")
+});
